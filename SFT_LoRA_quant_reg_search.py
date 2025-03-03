c6eda48937963bbbcda37433c957d644e778a8cf
Nevin <ngeorge4@stanford.edu>
2025-03-03 14:02:15 -0800

    Trying to fix bug.

--- SFT_LoRA_quant_reg_search.py
+++ SFT_LoRA_quant_reg_search.py
@@ -25,14 +25,6 @@ with open("results_SFT_LoRA_quant_reg_search.txt", "w") as file:
         print("Option (quantization, SAR):", option)
         file.write(f"Setting (quantization, SAR): {option}\n")
 
-        max_memory = {i: '46000MB' for i in range(torch.cuda.device_count())}
-        model = LlamaForCausalLM.from_pretrained(
-            base_model,
-            device_map="auto",
-            max_memory=max_memory
-        )
-        model.config.use_cache = False
-
         compute_dtype = getattr(torch, "float16")
 
         quant_config = BitsAndBytesConfig(
@@ -42,18 +34,35 @@ with open("results_SFT_LoRA_quant_reg_search.txt", "w") as file:
             bnb_4bit_use_double_quant=False,
         )
 
+        max_memory = {i: '46000MB' for i in range(torch.cuda.device_count())}
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            quantization_config=quant_config,
+            device_map="auto",
+            max_memory=max_memory
+        )
         model.quantization_config = quant_config
+        model.config.use_cache = False
 
         tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
         tokenizer.pad_token = tokenizer.eos_token
         tokenizer.padding_side = "right"
 
+        peft_params = LoraConfig(
+            lora_alpha=16,
+            lora_dropout=0.1,
+            r=64,
+            bias="none",
+            task_type="CAUSAL_LM",
+        )
         training_params = TrainingArguments(
             output_dir="./SFT-fine-tune-results",
             num_train_epochs=6,
             per_device_train_batch_size=4,
             gradient_accumulation_steps=1,
             optim="paged_adamw_32bit",
+            save_steps=25,
+            logging_steps=25,
             learning_rate=2e-4,
             weight_decay=0.001,
             fp16=False,
@@ -63,37 +72,17 @@ with open("results_SFT_LoRA_quant_reg_search.txt", "w") as file:
             warmup_ratio=0.03,
             group_by_length=True,
             lr_scheduler_type="constant",
-            report_to="none",
-            save_strategy="no",
-            logging_dir=None,
+            report_to="tensorboard",
             label_names=[str(i) for i in range(0, 11)]
         )
 
-        if option[1]: # Use SAR
-            trainer = SAR.SARTrainer(
-                model=model,
-                train_dataset=dataset,
-                tokenizer=tokenizer,
-                args=training_params,
-                epsilon=1e-3,   # Adjust perturbation magnitude
-                alpha=0.1       # Adjust SAR loss weight
-            )
-        else:
-            trainer = SFTTrainer(
-                model=model,
-                train_dataset=dataset,
-                tokenizer=tokenizer,
-                args=training_params
-            )
-
-        peft_params = LoraConfig(
-            lora_alpha=32,
-            lora_dropout=0.1,
-            r=16,
-            bias="none",
-            task_type="CAUSAL_LM",
+        trainer = SFTTrainer(
+            model=model,
+            train_dataset=dataset,
+            peft_config=peft_params,
+            tokenizer=tokenizer,
+            args=training_params
         )
-        trainer.peft_config = peft_params
 
         print("Fine-tuning model...")
         trainer.train()
