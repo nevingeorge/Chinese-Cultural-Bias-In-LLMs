37ab0b20c1e12ba0f04a1a9d8ac91b9124fc74be
Nevin <ngeorge4@stanford.edu>
2025-03-03 10:15:24 -0800

    Save results after each option.

--- SFT_LoRA_quant_reg_search.py
+++ SFT_LoRA_quant_reg_search.py
@@ -79,114 +79,110 @@ class SARTrainer(SFTTrainer):
 base_model = 'meta-llama/Llama-3.2-1B-Instruct'
 dataset = load_dataset("json", data_files="./data/WVQ_China_Train.jsonl", split="train")
 
-results = []
-
 # LoRA, quantization, SAR
 options = [[0,0,0], [1,0,0], [0,1,0], [0,0,1], [1,1,0], [1,0,1], [0,1,1], [1,1,1]]
 
-for option in options:
-    print("Option (LoRA, quantization, SAR):", option)
-
-    max_memory = {i: '46000MB' for i in range(torch.cuda.device_count())}
-    model = LlamaForCausalLM.from_pretrained(
-        base_model,
-        device_map="auto",
-        max_memory=max_memory
-    )
-    model.config.use_cache = False
-
-    if option[1]: # use quantization
-        compute_dtype = getattr(torch, "float16")
-
-        quant_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=compute_dtype,
-            bnb_4bit_use_double_quant=False,
-        )
-
-        model.quantization_config = quant_config
-
-    tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"
-
-    training_params = TrainingArguments(
-        output_dir="./SFT-fine-tune-results",
-        num_train_epochs=6,
-        per_device_train_batch_size=4,
-        gradient_accumulation_steps=1,
-        optim="paged_adamw_32bit",
-        learning_rate=2e-4,
-        weight_decay=0.001,
-        fp16=False,
-        bf16=False,
-        max_grad_norm=0.3,
-        max_steps=-1,
-        warmup_ratio=0.03,
-        group_by_length=True,
-        lr_scheduler_type="constant",
-        report_to="none",
-        save_strategy="no",
-        logging_dir=None,
-        label_names=[str(i) for i in range(0, 11)]
-    )
-
-    if option[2]: # Use SAR
-        trainer = SARTrainer(
-            model=model,
-            train_dataset=dataset,
-            tokenizer=tokenizer,
-            args=training_params,
-            epsilon=1e-3,   # Adjust perturbation magnitude
-            alpha=0.1       # Adjust SAR loss weight
-        )
-    else:
-        trainer = SFTTrainer(
-            model=model,
-            train_dataset=dataset,
-            tokenizer=tokenizer,
-            args=training_params
-        )
-
-    if option[0]: # Use LoRA
-        peft_params = LoraConfig(
-            lora_alpha=32,
-            lora_dropout=0.1,
-            r=16,
-            bias="none",
-            task_type="CAUSAL_LM",
-        )
-
-        trainer.peft_config = peft_params
-
-    print("Fine-tuning model...")
-    trainer.train()
-
-    print("Finished fine-tuning. Beginning evaluation...")
-
-    model = trainer.model
-    if hasattr(model, "merge_and_unload"):
-        model = model.merge_and_unload()
-
-    tokenizer = trainer.tokenizer
-
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        torch_dtype=torch.bfloat16,
-        device_map="auto",
-    )
-
-    final_score, total = evaluate_WVS_score.obtain_results("./data/WVQ_China_Evaluate.jsonl", pipe)
-
-    results.append((option, final_score, total))
-
 with open("results_SFT_LoRA_quant_reg_search.txt", "w") as file:
-    for result in results:
-        option, final_score, total = result
+    for option in options:
+        print("Option (LoRA, quantization, SAR):", option)
         file.write(f"Setting (LoRA, quantization, SAR): {option}\n")
+
+        max_memory = {i: '46000MB' for i in range(torch.cuda.device_count())}
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            device_map="auto",
+            max_memory=max_memory
+        )
+        model.config.use_cache = False
+
+        if option[1]: # use quantization
+            compute_dtype = getattr(torch, "float16")
+
+            quant_config = BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_compute_dtype=compute_dtype,
+                bnb_4bit_use_double_quant=False,
+            )
+
+            model.quantization_config = quant_config
+
+        tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = "right"
+
+        training_params = TrainingArguments(
+            output_dir="./SFT-fine-tune-results",
+            num_train_epochs=6,
+            per_device_train_batch_size=4,
+            gradient_accumulation_steps=1,
+            optim="paged_adamw_32bit",
+            learning_rate=2e-4,
+            weight_decay=0.001,
+            fp16=False,
+            bf16=False,
+            max_grad_norm=0.3,
+            max_steps=-1,
+            warmup_ratio=0.03,
+            group_by_length=True,
+            lr_scheduler_type="constant",
+            report_to="none",
+            save_strategy="no",
+            logging_dir=None,
+            label_names=[str(i) for i in range(0, 11)]
+        )
+
+        if option[2]: # Use SAR
+            trainer = SARTrainer(
+                model=model,
+                train_dataset=dataset,
+                tokenizer=tokenizer,
+                args=training_params,
+                epsilon=1e-3,   # Adjust perturbation magnitude
+                alpha=0.1       # Adjust SAR loss weight
+            )
+        else:
+            trainer = SFTTrainer(
+                model=model,
+                train_dataset=dataset,
+                tokenizer=tokenizer,
+                args=training_params
+            )
+
+        if option[0]: # Use LoRA
+            peft_params = LoraConfig(
+                lora_alpha=32,
+                lora_dropout=0.1,
+                r=16,
+                bias="none",
+                task_type="CAUSAL_LM",
+            )
+
+            trainer.peft_config = peft_params
+
+        print("Fine-tuning model...")
+        trainer.train()
+
+        print("Finished fine-tuning. Beginning evaluation...")
+
+        model = trainer.model
+        if hasattr(model, "merge_and_unload"):
+            model = model.merge_and_unload()
+
+        tokenizer = trainer.tokenizer
+
+        pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+        )
+
+        final_score, total = evaluate_WVS_score.obtain_results("./data/WVQ_China_Evaluate.jsonl", pipe)
+
+        print(f"WVS score: {final_score:.2f}%")
+        print(f"Total valid responses: {total}")
         file.write(f"WVS score: {final_score:.2f}%\n")
         file.write(f"Total valid responses: {total}\n")
         file.write("\n")
