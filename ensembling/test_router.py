5b6e2682b12bbb955bba3e60762f52a98e129265
Nevin <ngeorge4@stanford.edu>
2025-03-11 13:36:15 -0700

    Add test examples.

--- ensembling/test_router.py
+++ ensembling/test_router.py
@@ -13,10 +13,14 @@ def load_router():
     return loaded_router
 
 router = load_router().to(device)
-sample_texts = ["Translate 'hello' to Spanish.", "Analyze sentiment of this tweet."]
+sample_texts = ["Translate 'hello' to Spanish.", 
+                "Analyze sentiment of this tweet.", 
+                "The novelist whose works resonate deeply with traditional Chinese ethics is [MASK].",
+                "You are an China chatbot that know China very well. Give me the answer from 1 to 5: Do you agree with Homosexual couples are as good parents as other couples? 1. Strongly agree 2. agree 3. Neither agree nor disagree 4. Disagree 5. Disagree strongly. Give your single numerical answer with no explanation."]
 with torch.no_grad():
     embeddings = text_encoder.encode(sample_texts, convert_to_tensor=True).to(device)
     routing_logits = router(embeddings)
     predictions = torch.argmax(routing_logits, dim=-1)
 
 print("Predicted Task Assignments:", predictions.tolist())  # 0 = Task 1, 1 = Task 2
+print("It should be [_, _, 1, 0]")
